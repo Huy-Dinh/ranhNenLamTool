10ee29bb7a6db33924bd1a929669ea8818cd764d
Unknown <dinhquochuy.eeit@gmail.com>
2018-11-18 23:12:29 +0100

    Crude implementation for ScheduleTaskRunner

--- ranhNenLamToolProject/scheduletasksrunner.cpp
+++ ranhNenLamToolProject/scheduletasksrunner.cpp
@@ -8,24 +8,55 @@ ScheduleTasksRunner::ScheduleTasksRunner(QObject *parent,
     : QAbstractListModel(parent)
 {
     pItemView = itemView;
-    //pItemView->setModel(this);
+    pAddButton = AddButton;
+    pActivateButton = ActivateButton;
+    pRemoveButton = RemoveButton;
+
+    if (pItemView != NULL)
+    {
+        pItemView->setModel(this);
+    }
+    if (pAddButton != NULL)
+        connect(pAddButton, SIGNAL(clicked()), this, SLOT(addButtonClicked()));
+    if (pActivateButton != NULL)
+    {
+
+    }
+
+
 }
 
 int ScheduleTasksRunner::rowCount(const QModelIndex &parent) const
 {
-    return 2;
+    return mListOfTasks.count();
 }
 
 QVariant ScheduleTasksRunner::data(const QModelIndex &index, int role) const
 {
-    if (index.isValid())
+    if (!index.isValid())
+        return QVariant();
+
+    if (index.row() >= mListOfTasks.count())
+        return QVariant();
+
+    if (role == Qt::DisplayRole)
     {
-        return "AA";
+        ScheduledTask displayedTask = mListOfTasks.at(index.row());
+        QString displayedString = "";
+        if (displayedTask.mScheduleState == ScheduledTask::STATE_ACTIVE)
+        {
+            displayedString += "(A) ";
+        }
+        else
+        {
+            displayedString += "(D) ";
+        }
+        displayedString += displayedTask.mScheduledTime.toString();
+        displayedString += " ";
+        return displayedString;
     }
     else
-    {
-        QVariant();
-    }
+        return QVariant();
 }
 
 void ScheduleTasksRunner::viewIndexMoved(const QModelIndexList &indexes)
@@ -40,7 +71,9 @@ void ScheduleTasksRunner::activateButtonClicked()
 
 void ScheduleTasksRunner::addButtonClicked()
 {
-
+    beginInsertRows(QModelIndex(), mListOfTasks.count(),mListOfTasks.count());
+    mListOfTasks.append(ScheduledTask(QTime(10, 10, 10), "xadsdd", ScheduledTask::ACTION_OPEN));
+    endInsertRows();
 }
 
 void ScheduleTasksRunner::removeButtonClicked()
